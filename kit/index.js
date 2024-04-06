6963c7495320618a2677e28f0ac2cd96f1194ddf
Faddy Michel Samaan <faddy.michel@gmail.com>
2024-04-06 05:32:14 -0400

    @faddymichel: Kit now checks if link to a sample already exists in a kit. On branch development
    	modified:   kit/index.js
    
    Untracked files:
    
    	browser/
    	sequence/

--- kit/index.js
+++ kit/index.js
@@ -1,7 +1,8 @@
 import Scenarist from '@faddys/scenarist';
 import command from '@faddys/keyboardist/command';
-import { engine, kit as directory } from '@faddys/keyboardist/environment';
+import { engine, kit } from '@faddys/keyboardist/environment';
 import { parse } from 'node:path';
+import { createHash } from 'node:crypto';
 
 export const scenario = {
 
@@ -12,20 +13,33 @@ return $ ( ... process .argv .slice ( 2 ) );
 
 },
 
-async $_director ( $, path, kit ) {
+async $_director ( $, name, file ) {
 
-const { output: [ type ] } = await command ( `file --brief --mime-type ${ path }` );
+const { output: [ type ] } = await command ( `file --brief --mime-type ${ file }` );
 
 if ( typeof type === 'string' && ! type .startsWith ( 'audio/' ) )
-throw Error ( `${ path } is not an audio file` );
+throw Error ( `${ file } is not an audio file` );
 
-const link = `${ directory }/${ kit }/${ Date .now () }${ parse ( path ) .ext }`;
+let { dir: path, base, ext: extension } = parse ( file );
 
-await command ( `mkdir -p ${ directory }/${ kit }` );
-await command ( `ln -sr ${ path } ${ link }` );
-command ( `csound --smacro:sample=${ path } ${ engine }/kit/index.csd` );
+path = ( await command ( `cd ./${ path }; pwd` ) ) .output;
 
-console .log ( '#path', path );
+const hash = createHash ( 'sha256' ) .update ( `#kit/${ name } ${ path }/${ base }` ) .digest ( 'hex' );
+const directory = kit + '/' + name;
+let { output: [ link ] } = await command ( `cat ${ directory }/${ hash }` );
+
+if ( link )
+return console .log ( '#link', 'Already exists' );
+
+await command ( `mkdir -p ${ directory }` );
+
+link = `${ directory }/${ Date .now () }${ extension }`;
+
+command ( `ln -sr ${ file } ${ link }` );
+command ( `echo ${ link } > ${ directory }/${ hash }` );
+command ( `csound --smacro:sample=${ file } ${ engine }/kit/index.csd` );
+
+console .log ( '#sample', file );
 console .log ( '#link', link );
 
 }
